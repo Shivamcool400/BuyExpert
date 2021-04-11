0d10c8b577e99510e26d00fdf532cdea9016149e
Shivamcool400 <66465485+Shivamcool400@users.noreply.github.com>
2021-04-11 21:52:09 +0530

    carousel added for ravish bondu

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -12,6 +12,30 @@ class Home extends Component{
     </blockquote>
   </figure>
   <div><br /><br /></div>
+
+
+  <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="/gamingconsole.jpg" className="d-block w-100" alt="..." />
+    </div>
+    <div className="carousel-item">
+      <img src="/laptop.jpg" className="d-block w-100" alt="..." />
+    </div>
+    <div className="carousel-item">
+      <img src="/phone.jpg" className="d-block w-100" alt="..." />
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true" />
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true" />
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+
   
   <div className="row">
     <div className="col">
