b4b0ddcee535d1e48888112a0b5c5a9c502fa062
Goyal2001-30 <66236007+Goyal2001-30@users.noreply.github.com>
2021-04-11 17:47:03 +0530

    Footer completed

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,19 +6,48 @@ class Footer extends Component{
     render() {
         return (
 
-  
+  <div>
   <footer className="footer">
     <div className="footer-body">
-      <div className="footer-icons text-center">
-        <i class="fab fa-facebook footer-icon1"></i>
-        <i class="fab fa-instagram footer-icon2"></i>
-        <i class="fab fa-twitter footer-icon3"></i>
-        <i class="fab fa-linkedin footer-icon4"></i>
+      <div className="head3">BuyExperts</div>
+      <div className="main-footer">
+        <div className="footer-col">
+          <p>EXPLORE</p>
+          <div>Home</div>
+          <div>About us</div>
+          <div>Contact us</div>
+          <div>Login</div>
+        </div>
+        <div className="footer-col">
+          <p>ABOUT</p>
+          <div>Our story</div>
+          <div>Benefits</div>
+          <div>Team</div>
+          <div>Careers</div>
+        </div>
+        <div className="footer-col">
+          <p>LEGAL</p>
+          <div>Terms & Conditions</div>
+          <div>Privacy Policy</div>
+          <div>Terms of use</div>
+        </div>
       </div>
     </div>
+    <div className="copyright text-center">&copy; BUY EXPERTS. All rights reserved</div>
   </footer>
 
-        
+  <div>
+    <div className="footer-icons text-center">
+      <i class="fab fa-facebook footer-icon1"></i>
+      <i class="fab fa-instagram footer-icon2"></i>
+      <i class="fab fa-twitter footer-icon3"></i>
+      <i class="fab fa-linkedin footer-icon4"></i>
+    </div>
+  </div>
+
+  {/* <div className="copyright text-center">&copy; BUY EXPERTS. All rights reserved</div> */}
+
+        </div>
         
         );
     }
