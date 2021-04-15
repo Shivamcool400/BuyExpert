4bc88edf564298af1194147943b4a9a7561e81f7
Goyal2001-30 <66236007+Goyal2001-30@users.noreply.github.com>
2021-04-15 19:17:53 +0530

    Carousel completed

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -20,7 +20,7 @@ class Home extends Component{
    
   {/* <div className="text-center homehead">Our Motive...</div> */}
 
-  <div id="carouselExampleControls" className="carousel slide caro" data-bs-ride="carousel">
+  {/* <div id="carouselExampleControls" className="carousel slide caro" data-bs-ride="carousel">
   <div className="carousel-inner">
     <div className="carousel-item active">
       <img src="/homenew1.jpg" className="d-block w-100" alt="..." />
@@ -39,6 +39,42 @@ class Home extends Component{
   <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
     <span className="carousel-control-next-icon" aria-hidden="true" />
     <span className="visually-hidden">Next</span>
+  </button>
+</div> */}
+
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/homenew1-1.jpg" class="d-block w-100" alt="..." />
+      <div class="carousel-caption">
+        <h5 className="caption">Find your ideal device in just a few clicks.</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="/homenew2.jpg" class="d-block w-100" alt="..." />
+      <div class="carousel-caption">
+        <h5 className="caption">Utilize your money in the best way.</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="homenew3.jpg" class="d-block w-100" alt="..." />
+      <div class="carousel-caption">
+        <h5 className="caption">Buy the best product that satisfies your needs.</h5>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
   </button>
 </div>
 
