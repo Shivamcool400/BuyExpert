b4c4c2e9da05b6069d0f5c4be55613685e74880d
Shivamcool400 <66465485+Shivamcool400@users.noreply.github.com>
2021-04-22 22:49:12 +0530

    new commit

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -17,8 +17,7 @@ function Navbar() {
 
         const handleLogout = () => {
                 Fire.auth().signOut();
-                history.push('/login')
-        };
+                };
 
         const [click,setClick] = useState(false);
         const [dropdown,setDropdown] = useState(false);
