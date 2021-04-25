851cbd3d012f17a36f31ec3224cf810cf138d5a8
Goyal2001-30 <66236007+Goyal2001-30@users.noreply.github.com>
2021-04-25 19:44:52 +0530

    Our team made successfully

--- src/components/pages/ourteam.js
+++ src/components/pages/ourteam.js
@@ -6,36 +6,36 @@ function Ourteam() {
         <div>
            <>
            <blockquote className="blockquote ">
-          <h2 className="text_heading text-dark">Our Team</h2>
+          <h2 className="text_heading text-dark team-head">Our Team</h2>
       </blockquote>
-  <div className="row">
+  <div className="row team_start">
     <div className="column">
-      <div className="card">
-        <img src="/shivam.jpg" alt="shivam" style={{width: '100%'}} />
+      <div className="card team-image">
+        <img src="/shivam.jpg" alt="shivam" />
         <div className="container">
           <h2>Shivam Gupta</h2>
-          <p className="title">Founder&amp; Backend/Frontend Developer</p>
+          <p className="title">Founder &amp; Web Developer</p>
           <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-          <p>Shivamcool400@gmail.com</p>
+          <p>shivamcool400@gmail.com</p>
           <p><button className="button">Contact</button></p>
         </div>
       </div>
     </div>
     <div className="column">
-      <div className="card">
-        <img src="/w3images/team2.jpg" alt="Mike" style={{width: '100%'}} />
+      <div className="card team-image">
+        <img src="/ravish.jpg" alt="Ravish" />
         <div className="container">
-          <h2>Mike Ross</h2>
-          <p className="title">Art Director</p>
+          <h2>Ravish Goyal</h2>
+          <p className="title">Web Developer &amp; Debugger</p>
           <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-          <p>mike@example.com</p>
+          <p>goyalravish2001@gmail.com</p>
           <p><button className="button">Contact</button></p>
         </div>
       </div>
     </div>
     <div className="column">
-      <div className="card">
-        <img src="/bral.jpeg" alt="bral" style={{width: '100%'}} />
+      <div className="card team-image">
+        <img src="/bral.jpeg" alt="bral" />
         <div className="container">
           <h2>Anurag Bral</h2>
           <p className="title">Data Manager and Analyzer</p>
