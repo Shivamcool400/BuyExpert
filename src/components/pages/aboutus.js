0f294b2b085044fce7e97541eef223ad23812955
Shivamcool400 <66465485+Shivamcool400@users.noreply.github.com>
2021-07-28 01:26:18 +0530

    some changes

--- src/components/pages/aboutus.js
+++ src/components/pages/aboutus.js
@@ -15,7 +15,7 @@ class Aboutus extends Component{
                   <img src="/ourvision.webp" alt="Our Vision" className="image1" />
                 </div>
                 <div className="para1">
-                  <h3>Our Vision</h3>
+                  <h3>Vision</h3>
                   <p>Buy ideal's vision is to be the technical advancement in the nation. Due to an increase in demands of advance gadgets, we as a company visualise suggesting you no complain technology in subsequent era. As of now, many of us are having choice issues, so we come down as a solution to all your choice issues. We aim at helping you, pick up an ideal choice and at the same time recommending you a complain free technology.</p>
                 </div>
       
@@ -25,8 +25,8 @@ class Aboutus extends Component{
                 <div>
                   <img src="/ourapproach.webp" alt="Our Approach" className="image2" /></div>
                 <div className="para2">
-                  <h3>Our Approach</h3>
-                  <p>Buyideal, approaching to its major goals provide you with a single platform to pick up the best technology based upon our researches, be it be mobile phones, laptops or other useful gadgets. Our approach to contend with your choices utilizes a lot of researches. Stepping into buyideal will let you have a sight on all kinds of mobile phones, laptops and other available gadgets. Keeping your budget specifications and other criterias in mind, we ensure helping you in making the finest deal.</p>
+                  <h3>Approach</h3>
+                  <p>Buyideal, approaching to its major goals provide you with a single platform to pick up the best technology based upon our researches, be it be mobile phones, laptops or other useful gadgets. Our approach to contend with your choices utilizes a lot of researches. Stepping into buyideal will let you have a sight on all kinds of mobile phones, laptops and other available gadgets.we ensure helping you in making the finest deal.</p>
                 </div>
               </div>
       
@@ -34,7 +34,7 @@ class Aboutus extends Component{
                 <div>
                   <img src="/ourprocess.webp" alt="Our Process" className="image3" /></div>
                 <div className="para3">
-                  <h3>Our Values</h3>
+                  <h3>Values</h3>
                   <p>In today's world, we believe in communicating and working transparently following integrity. Being involve in a business, we are very much passionate about meeting our customer's requirement. We work as team and are accountable for all our actions and we are dedicated to achieve our goals with a token of respect for each other.</p>
                 </div>
               </div>
--- src/components/pages/ourteam.js
+++ src/components/pages/ourteam.js
@@ -5,74 +5,82 @@ function Ourteam() {
     return (
         <div>
            <>
+           <br></br>
            <blockquote className="blockquote ">
-          <h2 className="text_heading text-dark team-head">Our Team</h2>
+          <h2 className="text_heading text-dark team-head">Management</h2>
       </blockquote>
-  <div className="row team_start">
-    <div className="column">
+           <div className="row team_start">
+             <div className='col'></div>
+           <div className="column">
       <div className="card team-image">
         <img src="/shivam.webp" className="img-fluid" alt="shivam" />
         <div className="container">
           <h2>Shivam Gupta</h2>
           <p className="title">Founder &amp; Web Developer</p>
-          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+          {/* <p>Some text that describes me lorem ipsum ipsum lorem.</p> */}
           <p>shivamcool400@gmail.com</p>
           <p><button className="button">Contact</button></p>
         </div>
       </div>
     </div>
-    <div className="column">
+    
+    <div className='col'></div>
+    </div>
+           
+           <blockquote className="blockquote ">
+          <h2 className="text_heading text-dark team-head">Contributors</h2>
+      </blockquote>
+      <div className="row team_start">
+    
+    <div className='col'></div>
+      <div className="column">
       <div className="card team-image">
         <img src="/ravish.webp" className="img-fluid" alt="Ravish" />
         <div className="container">
           <h2>Ravish Goyal</h2>
           <p className="title">Web Developer &amp; Debugger</p>
-          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+          {/* <p>Some text that describes me lorem ipsum ipsum lorem.</p> */}
           <p>goyalravish2001@gmail.com</p>
           <p><button className="button">Contact</button></p>
         </div>
       </div>
     </div>
-    <div className="column">
-      <div className="card team-image">
-        <img src="/bral.webp" className="img-fluid" alt="bral" />
-        <div className="container">
-          <h2>Anurag Bral</h2>
-          <p className="title">Data Manager and Analyzer</p>
-          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-          <p>Anuragbral2127@gmail.com</p>
-          <p><button className="button">Contact</button></p>
-        </div>
-      </div>
+    <div className='col'></div>
     </div>
-
-    <div className="row team_start">
+  <div className="row team_start">
+    
+  <div className='col'></div>
     <div className="column">
       <div className="card team-image">
         <img src="/bindu.webp" className="img-fluid" alt="shivam" />
         <div className="container">
           <h2>Bindu Bharti</h2>
           <p className="title">Content Writer</p>
-          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+          {/* <p>Some text that describes me lorem ipsum ipsum lorem.</p> */}
           <p>bindubharti264@gmail.com</p>
           <p><button className="button">Contact</button></p>
         </div>
       </div>
     </div>
+    <div className='col'></div>
+    </div>
+    <div className="row team_start">
+    <div className='col'></div>
     <div className="column">
       <div className="card team-image">
         <img src="/surmeet.webp" className="img-fluid" alt="Ravish" />
         <div className="container">
           <h2>Surmeet Kaur</h2>
           <p className="title">Content Writer</p>
-          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+         {/*  <p>Some text that describes me lorem ipsum ipsum lorem.</p> */}
           <p>surmeetsehgal@gmail.com</p>
           <p><button className="button">Contact</button></p>
         </div>
       </div>
     </div>
+    <div className='col'></div>
     
-</div>
+
   </div>
 </>
  
